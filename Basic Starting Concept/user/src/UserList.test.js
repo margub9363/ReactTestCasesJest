d94de3113a4179f8c6652b57d1d1e1019e755095
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-20 17:49:19 +0530

    L22 L23 QuerySelector and Testing PlayGround

---
+++ Basic Starting Concept/user/src/UserList.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import UserList from "./UserList";
+
+test("render one row per user", () => {
+  const users = [
+    { name: "rahman", email: "rahaman@gmail.com" },
+    { name: "Tannu", email: "Tannu@gmail.com" },
+  ];
+  //   Render the componet
+  const { container } = render(<UserList users={users} />);
+  // Find all the rows in a table
+  const rows = container.querySelectorAll("tbody tr");
+  // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+  //   screen.logTestingPlaygroundURL(); //this will generate the link to testing playground
+});
+
+test("render the email and name of each user", () => {});
