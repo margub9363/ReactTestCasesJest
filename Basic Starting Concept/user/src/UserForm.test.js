eff561dcf9a404f1182181a10c450e79be9f6711
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-20 12:44:14 +0530

    11 Correction

--- Basic Starting Concept/user/src/UserForm.test.js
+++ Basic Starting Concept/user/src/UserForm.test.js
@@ -8,13 +8,12 @@ test("It shows two input and a button", () => {
 
   //   Manipulate the comnponent or find an element in it
   const inputs = screen.getAllByRole("textbox");
-  const button = screen.getAllByRole("button");
+  const button = screen.getByRole("button");
 
   //   Assertion - make sure the component is doing
 
   //   What we expect to do
   expect(inputs).toHaveLength(2);
   //   console.log(button);
-  // expect(button).toBeInTheDocument(); this is not passing now  don't know why
-  expect(button).toHaveLength(1);
+  expect(button).toBeInTheDocument();
 });
