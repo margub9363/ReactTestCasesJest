64143c649cd6536c49703688d8fef094eabb6e03
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-20 13:30:17 +0530

    17 Recording Function Calls

--- Basic Starting Concept/user/src/UserForm.test.js
+++ Basic Starting Concept/user/src/UserForm.test.js
@@ -17,3 +17,28 @@ test("It shows two input and a button", () => {
   //   console.log(button);
   expect(button).toBeInTheDocument();
 });
+
+test(" it call onUserAdd when the form is submitted", () => {
+  // Not the best implementation
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+  // Try to render my component
+  render(<UserForm onUserAdd={callback} />);
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  // simulate typing in name
+  user.click(nameInput);
+  user.keyboard("Rahman");
+  // simulate typing in email
+  user.click(emailInput);
+  user.keyboard("rahman@gmail.com");
+  // Find the button
+  const button = screen.getByRole("button");
+  // Simulate clicking the button
+  user.click(button);
+  // Asserion to make sure 'onUserAdd' is getting callend with email and name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "Rahman", email: "rahman@gmail.com" });
+});
