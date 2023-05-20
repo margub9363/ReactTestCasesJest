1c5039146ecb9c99f67998b0e27610d95d057b06
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-20 13:49:22 +0530

    15 Making User interaction async and await

--- Basic Starting Concept/user/src/UserForm.test.js
+++ Basic Starting Concept/user/src/UserForm.test.js
@@ -18,7 +18,7 @@ test("It shows two input and a button", () => {
   expect(button).toBeInTheDocument();
 });
 
-test(" it call onUserAdd when the form is submitted", () => {
+test(" it call onUserAdd when the form is submitted", async () => {
   // Not the best implementation
   const argList = [];
   const callback = (...args) => {
@@ -29,15 +29,15 @@ test(" it call onUserAdd when the form is submitted", () => {
   // Find the two inputs
   const [nameInput, emailInput] = screen.getAllByRole("textbox");
   // simulate typing in name
-  user.click(nameInput);
-  user.keyboard("Rahman");
+  await user.click(nameInput);
+  await user.keyboard("Rahman");
   // simulate typing in email
-  user.click(emailInput);
-  user.keyboard("rahman@gmail.com");
+  await user.click(emailInput);
+  await user.keyboard("rahman@gmail.com");
   // Find the button
   const button = screen.getByRole("button");
   // Simulate clicking the button
-  user.click(button);
+  await user.click(button);
   // Asserion to make sure 'onUserAdd' is getting callend with email and name
   expect(argList).toHaveLength(1);
   expect(argList[0][0]).toEqual({ name: "Rahman", email: "rahman@gmail.com" });
