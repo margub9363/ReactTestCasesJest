16bced89385ebc4af1ca9643d59c6570eb19f95f
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-17 19:00:36 +0530

    Testing Methof of Functional Component by extracting a method

--- Basic Starting Concept/user/src/App.js
+++ Basic Starting Concept/user/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import User from "./User";
+import "@testing-library/jest-dom";
+import handleOtherMethod from "./helper";
 
 // https://www.youtube.com/watch?v=EfdveVHHLnA&list=PL8p2I9GklV47ZpFC9sNHTXwJRgwwzdkNG&index=9
 function App() {
@@ -8,6 +10,11 @@ function App() {
     setData("hello");
   }
 
+  // Now we are going to extract this method in a separate file
+  // const handleOtherMethod = () => {
+  //   console.log("Hi");
+  // };
+
   return (
     <div className="App">
       <h1>Functional Component Method Testing - Part 1</h1>
@@ -15,8 +22,14 @@ function App() {
         Update
       </button>
       <h1>{data}</h1>
+
+      <button onClick={handleOtherMethod}>Print</button>
     </div>
   );
 }
 
 export default App;
+
+test("Method Testing Case2", () => {
+  expect(handleOtherMethod()).toMatch("hi");
+});
